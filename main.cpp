47da5bc17ac13beba8c27f3bfc538e405e37ddc9
ashishpandey2600 <ashishpandey@Ashishs-MacBook-Pro.local>
2022-12-04 07:18:16 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int SPEED_LIMIT = 10;
+double TRAFFIC_DESITY = 0.15;//Make inputs
+int MAX_ACCEL = 2;//Make inputs
+vector<int > LANE_SPEEDS = {6,7,8,9}; //Make inputs
+vector<int > GOAL = {300,0}; //Make inputs
+
+int FRAMES_PER_SECOND = 4;
+int AMOUT_OF_ROAD_VISIBLE = 40;
+
+
+
+
+int main(){
+    cout<<"ALL OK"<<endl;
+    
+}
